c3cfbaf636baf6052e3d7092700a347cbc503516
Winson Gin <35970210+winsongin@users.noreply.github.com>
2020-05-06 13:48:00 -0700

    edit scripts to test different value

--- scripts/get_existing.sh
+++ scripts/get_existing.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # GET request that retrieves an existing post using the postID and community as arguments
-curl --request GET "http://localhost:5000/api/v1.0/resources/collections?postID=0&community=CSUF"
+curl --request GET "http://localhost:5000/api/v1.0/resources/collections?postID=4&community=CSUF"
